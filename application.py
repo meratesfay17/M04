6c0d9dbb7cba4010a81053ee0fc798c7da450141
meratesfay17 <143534779+meratesfay17@users.noreply.github.com>
2024-04-22 14:03:29 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+    publisher = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f"Book('{self.book_name}', '{self.author}', '{self.publisher}')"
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+    output = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'book_name': book.book_name,
+            'author': book.author,
+            'publisher': book.publisher
+        }
+        output.append(book_data)
+    return {'books': output}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
